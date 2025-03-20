f6fe6d75e1afb7136dc94653642890ea8d45cb8d
Pablo Câmara <pablo.camara.pt@gmail.com>
2025-03-20 19:23:50 +0000

    splitting up some components for reusability

---
+++ src/components/LatestPosts.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="latest-posts">
+        <h2>Latest Posts</h2>
+
+        <div class="latest-post">
+            <h3>Post Title</h3>
+            <p>Post Content</p>
+        </div>
+
+        <div class="latest-post">
+            <h3>Post Title 2</h3>
+            <p>Post Content 2</p>
+        </div>
+
+        <div class="latest-post">
+            <h3>Post Title 3</h3>
+            <p>Post Content 3</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .latest-posts {
+        max-width: 300px;
+        margin: auto;
+        margin-top: 40px;
+    }
+
+    .latest-posts h2 {
+        color: #ffffff
+    }
+    
+
+    .latest-post h3 {
+        margin-bottom: 0;
+        margin-top: 0;
+    }
+
+    .latest-post {
+        background-color: #ffffff;
+        padding: 10px;
+        margin-top: 10px;
+        border-radius: 5px;
+    }
+</style>
--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRouter } from 'vue-router';
 import HeaderBox from '@/components/HeaderBox.vue';
+import LatestPosts from '@/components/LatestPosts.vue';
 
 const router = useRouter();
 
@@ -21,29 +22,10 @@ const goToRegisterPage = () => {
         <button class="white-btn" @click="goToRegisterPage">Register</button>
     </div>
 
-    <div class="latest-posts">
-        <h2>Latest Posts</h2>
-    
-        <div class="latest-post">
-            <h3>Post Title</h3>
-            <p>Post Content</p>
-        </div>
-
-        <div class="latest-post">
-            <h3>Post Title 2</h3>
-            <p>Post Content 2</p>
-        </div>
-
-        <div class="latest-post">
-            <h3>Post Title 3</h3>
-            <p>Post Content 3</p>
-        </div>
-    </div>
+    <LatestPosts />
 </template>
 
 <style scoped>
-
-    
 
     .authentication {
         text-align: center;
@@ -65,27 +47,5 @@ const goToRegisterPage = () => {
     .login-btn {
         margin-right: 10px;
     }
-
-    .latest-posts {
-        max-width: 300px;
-        margin: auto;
-        margin-top: 40px;
-    }
-
-    .latest-posts h2 {
-        color: #ffffff
-    }
     
-
-    .latest-post h3 {
-        margin-bottom: 0;
-        margin-top: 0;
-    }
-
-    .latest-post {
-        background-color: #ffffff;
-        padding: 10px;
-        margin-top: 10px;
-        border-radius: 5px;
-    }
 </style>
