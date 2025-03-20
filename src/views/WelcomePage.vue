07b137a729adbd2f2eff0e06f1731d754e38218d
Pablo Câmara <pablo.camara.pt@gmail.com>
2025-03-20 18:14:04 +0000

    login and register btn initial

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -8,6 +8,11 @@ import { ref } from 'vue';
         <h1>Welcome</h1>
         <p>Simplicity is the ultimate sophistication.</p>
     </div>
+
+    <div class="authentication">
+        <button class="white-btn login-btn">Login</button>
+        <button class="white-btn">Register</button>
+    </div>
 </template>
 
 <style scoped>
@@ -18,4 +23,24 @@ import { ref } from 'vue';
         text-align: center;
     }
 
+    .authentication {
+        text-align: center;
+    }
+
+    .white-btn {
+        background-color: #ffffff;
+        color: #000000;
+        border: none;
+        padding: 10px 20px;
+        cursor: pointer;
+        margin-top: 10px;
+    }
+
+    .white-btn:hover {
+        background-color: #00ff00;
+    }
+
+    .login-btn {
+        margin-right: 10px;
+    }
 </style>
