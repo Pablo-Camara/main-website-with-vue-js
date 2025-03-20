7fb7eacad322af9d9262e20c4b19e3d0ad48d12d
Pablo Câmara <pablo.camara.pt@gmail.com>
2025-03-20 22:21:11 +0000

    rename generic css file to utils

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/css/forms.css';
+import './assets/css/utils.css';
 
 const app = createApp(App)
 
