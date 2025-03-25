d7ccefe671003805698ba2819b7232c02c7d29e3
Pablo Câmara <pablo.camara.pt@gmail.com>
2025-03-25 00:26:48 +0000

    format with Vue Official formatter

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -71,20 +71,25 @@ const register = () => {
     <HeaderBox title="Register" subtitle="Create your account and join us!" />
 
     <div class="form-box">
-        <BaseInput v-model="firstName" label="First name" id="firstName" type="text" placeholder="Ex: John"/>
-        <br/><br/>
-        <BaseInput v-model="lastName" label="Last name" id="lastName" type="text" placeholder="Ex: Doe"/>
-        <br/><br/>
-        <BaseInput v-model="email" label="Email" id="email" type="email" placeholder="your@email.com" disableClipboard="true" :error="formErrors.value?.email || null"/>
-        <br/><br/>
-        <BaseInput v-model="emailConfirm" label="Repeat Email" id="emailConfirm" type="email" placeholder="your@email.com" disableClipboard="true"/>
-        <br/><br/>
-        <BaseInput v-model="password" label="Create Password" id="password" type="password" placeholder="***********" disableClipboard="true"/>
-        <br/><br/>
-        <BaseInput v-model="passwordConfirm" label="Repeat Password" id="passwordConfirm" type="password" placeholder="***********" disableClipboard="true"/>
-        <br/><br/>
-        <BaseCoregBinary text="Do you agree to receive news and updates via email?" :confirmed="newsletterConfirm" @confirm="setNewsletterConfirm" />
-        <br/><br/>
+        <BaseInput v-model="firstName" label="First name" id="firstName" type="text" placeholder="Ex: John" />
+        <br /><br />
+        <BaseInput v-model="lastName" label="Last name" id="lastName" type="text" placeholder="Ex: Doe" />
+        <br /><br />
+        <BaseInput v-model="email" label="Email" id="email" type="email" placeholder="your@email.com"
+            disableClipboard="true" :error="formErrors.value?.email || null" />
+        <br /><br />
+        <BaseInput v-model="emailConfirm" label="Repeat Email" id="emailConfirm" type="email"
+            placeholder="your@email.com" disableClipboard="true" />
+        <br /><br />
+        <BaseInput v-model="password" label="Create Password" id="password" type="password" placeholder="***********"
+            disableClipboard="true" />
+        <br /><br />
+        <BaseInput v-model="passwordConfirm" label="Repeat Password" id="passwordConfirm" type="password"
+            placeholder="***********" disableClipboard="true" />
+        <br /><br />
+        <BaseCoregBinary text="Do you agree to receive news and updates via email?" :confirmed="newsletterConfirm"
+            @confirm="setNewsletterConfirm" />
+        <br /><br />
         <p class="error" v-if="genericError">{{ genericError }}</p>
         <BaseButton class="mb-10 full" @click="register">Register</BaseButton>
         <BaseButton class="red-alert" @click="goHome">Back</BaseButton>
