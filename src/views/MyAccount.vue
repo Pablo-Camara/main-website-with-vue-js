411e2e7031fff81abdbb4ce96f0894bd0ed3c8a1
Pablo Câmara <pablo.camara.pt@gmail.com>
2025-03-26 11:30:03 +0000

    unused imports

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,11 +1,5 @@
 <script setup>
-import { useRouter } from 'vue-router';
 import HeaderBox from '@/components/HeaderBox.vue';
-import LatestPosts from '@/components/LatestPosts.vue';
-import BaseButton from '@/components/BaseComponents/BaseButton.vue';
-import { useNavigation } from '@/utils/routerUtils';
-
-const { goToLoginPage, goToRegisterPage } = useNavigation();
 
 </script>
 
