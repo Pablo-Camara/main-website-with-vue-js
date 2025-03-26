ebc6f795e5efbee2968c5a705b349619fa2c0f14
Pablo Câmara <pablo.camara.pt@gmail.com>
2025-03-26 22:41:40 +0000

    check auth on app mounted

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import { RouterView } from 'vue-router'
 import PabloCamaraLoader from './components/PabloCamaraLoader.vue'
+import { useAuthStore } from './stores/useAuthStore'
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.checkAuth();
+})
 </script>
 
 <template>
